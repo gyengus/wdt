4c86ef8b9168bd3041bc4b3107cf5abd1d71d298
gyengus <gyengus@gmail.com>
2018-08-28 10:30:43 +0200

    Initial commit

---
+++ config.example.h
@@ -0,0 +1,19 @@
+#define LED 1
+#define RELAY 0
+#define DEBUG 0
+
+#define SYSLOG_SERVER ""
+#define SYSLOG_PORT 514
+#define DEVICE_HOSTNAME "wdt"
+#define APP_NAME "wdt"
+
+#define HOST ""
+#define PING_INTERVALL 60 /* in seconds */
+#define PING_NUM 2
+#define PING_RETRY_NUM 3
+
+const char *sta_ssid = "";
+const char *sta_password = "";
+
+const char *maker_api_key = "";
+const char *maker_event = "";
---
+++ wdt.ino
@@ -0,0 +1,182 @@
+#include "config.h"
+
+#include <ESP8266WiFi.h>
+#include <ESP8266NetBIOS.h>
+#include <ESP8266Ping.h>
+#include <WiFiUdp.h>
+#include <Syslog.h>
+#include <Ticker.h>
+
+WiFiClient espClient;
+WiFiUDP udpClient;
+Ticker ticker;
+
+Syslog syslog(udpClient, SYSLOG_SERVER, SYSLOG_PORT, DEVICE_HOSTNAME, APP_NAME, LOG_INFO);
+String macAddress = "";
+long lastWiFiReconnectAttempt = 0;
+int failedPings = 0;
+bool needCheck = false;
+
+void setNeedCheck();
+void checkHost();
+bool pingHost();
+void resetHost();
+bool connectToWiFi();
+void sendNotification(String message);
+
+void setup() {
+  ESP.wdtDisable();
+  pinMode(RELAY, OUTPUT);
+
+  if (DEBUG) {
+    Serial.begin(115200);
+    delay(1000);
+    Serial.println("");
+    macAddress = WiFi.macAddress();
+    Serial.print("MAC address: ");
+    Serial.println(macAddress);
+  } else {
+    pinMode(LED, OUTPUT);
+  }
+
+  NBNS.begin(DEVICE_HOSTNAME);
+  connectToWiFi();
+
+  ticker.attach(PING_INTERVALL, setNeedCheck);
+
+  ESP.wdtEnable(15000);
+
+  String buf = "Started, watching host: " + String(HOST);
+  syslog.logf(LOG_INFO, buf.c_str());
+}
+
+void loop() {
+  ESP.wdtFeed();
+  if (WiFi.status() == WL_CONNECTED) {
+    if (needCheck) {
+      needCheck = false;
+      checkHost();
+    }
+  } else {
+    long now = millis();
+    if (now - lastWiFiReconnectAttempt > 5000) {
+      if (connectToWiFi()) {
+        lastWiFiReconnectAttempt = 0;
+      } else {
+        lastWiFiReconnectAttempt = millis();
+      }
+    }
+  }
+  ESP.wdtFeed();
+  delay(500);
+}
+
+void resetHost() {
+  ESP.wdtFeed();
+  ticker.detach();
+
+  digitalWrite(RELAY, HIGH);
+  if (DEBUG) Serial.println("Relay high");
+  delay(1000);
+  digitalWrite(RELAY, LOW);
+  if (DEBUG) Serial.println("Relay low");
+  syslog.logf(LOG_ERR, "Host restarted");
+  sendNotification("Host restarted");
+
+  ticker.attach(PING_INTERVALL, setNeedCheck);
+}
+
+void setNeedCheck() {
+  needCheck = true;
+}
+
+void checkHost() {
+  if (!pingHost()) {
+    failedPings++;
+    if (failedPings >= PING_RETRY_NUM) {
+      syslog.logf(LOG_ERR, "Host unreachable");
+      if (DEBUG) Serial.println("Host not reachable");
+      failedPings = 0;
+      resetHost();
+    }
+  }
+}
+bool pingHost() {
+  bool success;
+  if (!DEBUG) digitalWrite(LED, LOW);
+  if (success = Ping.ping(HOST, PING_NUM)) {
+    if (DEBUG) Serial.println("Ping OK");
+  } else {
+    if (DEBUG) Serial.println("Ping failed");
+    syslog.logf(LOG_ERR, "Ping failed");
+  }
+  if (!DEBUG) digitalWrite(LED, HIGH);
+  return success;
+}
+
+bool connectToWiFi() {
+  WiFi.mode(WIFI_STA);
+  if (DEBUG) {
+    Serial.print("Connecting to WiFi network: ");
+    Serial.println(sta_ssid);
+  }
+  WiFi.hostname(DEVICE_HOSTNAME);
+  WiFi.begin(sta_ssid, sta_password);
+
+  int i = 0;
+  while (WiFi.status() != WL_CONNECTED) {
+    ESP.wdtFeed();
+    delay(500);
+    if (DEBUG) Serial.print(".");
+    i++;
+    if (i >= 20) break;
+  }
+  if (DEBUG) Serial.println("");
+
+  if (WiFi.status() == WL_CONNECTED) {
+    if (DEBUG) {
+      Serial.print("Success, IP address: ");
+      Serial.println(WiFi.localIP());
+    }
+    return true;
+  } else {
+    if (DEBUG) Serial.println("Unable to connect");
+    return false;
+  }
+}
+
+void sendNotification(String message) {
+  WiFiClient client;
+
+  if (client.connect("maker.ifttt.com", 80)) {
+    String body = "{\"value1\": \"[" + String(DEVICE_HOSTNAME) + "] " + message + "\"}";
+    if (DEBUG) {
+      Serial.println("Url: maker.ifttt.com/trigger/" + String(maker_event) + "/with/key/" + String(maker_api_key));
+      Serial.println("Connected to remote host, sending data: " + body + "\n");
+    }
+
+    client.println("POST /trigger/" + String(maker_event) + "/with/key/" + String(maker_api_key) + " HTTP/1.1");
+    client.println("Host: maker.ifttt.com");
+    client.println("Content-Type: application/json");
+    if (!DEBUG) client.println("Connection: close"); // Ha ezt küldöm, akkor a szerver válasz nélkül bontja a kapcsolatot.
+    client.println("Content-Length: " + String(body.length()));
+    client.println();
+    client.println(body);
+    client.println();
+    delay(1000);
+
+    if (DEBUG) {
+      Serial.println("IFTTT answer:");
+      char c;
+      while (client.available()){
+        c = client.read();
+        Serial.print(c);
+      }
+      Serial.println();
+    }
+
+    client.stop();
+  } else {
+    if (DEBUG) Serial.println("Error iftttMessage");
+  }
+}
